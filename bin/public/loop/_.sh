3ac09c54a4516100d6e68ca461bdfd86dd472eaa
da99 <i-hate-spam-1234567@mailinator.com>
2017-08-18 19:50:37 -0500

    Added: loop every SLEEP max DD or until CMD

---
+++ bin/public/loop/_.sh
@@ -0,0 +1,31 @@
+#
+# === {{CMD}} every SLEEP max X or until MY CMD --ARGS
+#
+
+{{NAME}} () {
+  local +x origin="$@"
+
+  case "$(echo $@ | xargs echo -n)" in
+    "every "*" max "*" or until "*)
+      shift; local +x SLEEP="$1"
+      shift; local +x MAX="$1"
+      shift; shift; local +x CMD="$@"
+      if [[ "$MAX" -lt 1 ]]; then
+        echo "!!! Invalid max, $MAX, for: loop $origin" >&2
+        exit 5
+      fi
+      local +x COUNT=0
+      while [[ "$COUNT" -lt "$MAX" ]]; do
+        sleep $SLEEP
+        COUNT="$((COUNT + 1))"
+        $CMD && return 0 || continue
+      done
+      exit 5
+      ;;
+
+    *)
+      echo "!!! Invalid command: $origin" >&2
+      exit 1
+      ;;
+  esac
+} # {{NAME}} ()
