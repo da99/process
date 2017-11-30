8bff6dd97da01914e9a6a137f5e1c9cbed09def9
da99 <i-hate-spam-1234567@mailinator.com>
2017-11-29 20:46:01 -0600

    Various fixes.

--- bin/public/group-of/_.sh
+++ bin/public/group-of/_.sh
@@ -3,5 +3,5 @@
 # ===   FROM: https://coderwall.com/p/q-ovnw/killing-all-child-processes-in-a-shell-script
 group-of () {
   local +x PID="$1"; shift
-  ps -o pgid= $PID | grep -o [0-9]*
+  ps -o pgid= $PID | grep -o '[0-9]*'
 } # === end function
